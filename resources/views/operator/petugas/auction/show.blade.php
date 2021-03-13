6d657ded91abd1b357b06b63a801d2a74b302345
Tubagus Gusti Fauzy <fravens26@gmail.com>
2021-03-13 14:32:19 +0700

    ubah label author ke operator, tambah tabel pelelang di show auction

--- resources/views/operator/petugas/auction/show.blade.php
+++ resources/views/operator/petugas/auction/show.blade.php
@@ -147,7 +147,7 @@
           {{ $auction->item->desc }}
         </dd>
       </div>
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">
           Status
         </dt>
@@ -174,7 +174,7 @@
       </div>
       <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">
-          Author
+          Operator
         </dt>
         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
           {{ $auction->item->operator->name }}
@@ -182,6 +182,111 @@
       </div>
 
     </dl>
+  </div>
+</div>
+
+<div class="px-4">
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="overflow-hidden sm:rounded-lg">
+        <div class="p-6">
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            #
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Penawar
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          No. Handphone
+                          </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Harga Penawaran
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                        </th>
+                        <th scope="col" class="relative px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($auction->item->bids as $data)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {{ $loop->iteration }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="flex items-center">
+                            <div class="flex-shrink-0 h-10 w-10">
+                            @if ($data->people->user->getMedia('avatar')->count() === 0)
+                            <img class="h-10 w-10 rounded-full" src="{{ asset('image/download.png') }}" alt="">
+                            @else
+                            <img class="h-10 w-10 rounded-full" src="{{ $data->people->user->getMedia('avatar')[0]->getUrl() }}" alt="">   
+                            @endif
+                            </div>
+                            <div class="ml-4">
+                            <div class="text-sm font-medium text-gray-900">
+                                {{ $data->people->name }}
+                            </div>
+                            <div class="text-sm text-gray-500">
+                                {{ $data->people->user->email }}
+                            </div>
+                            </div>
+                        </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            {{ $data->people->phone }}
+                        </span>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        @currency($data->bid_price)
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        @if ($auction->bid === null)
+                          tunda
+                        @else
+                          @if ($auction->bid->people->name === $data->people->name)
+                          <i class="fas fa-crown text-yellow-400 mr-1"></i>
+                          @else
+                          <i class="fas fa-frown text-red-400 mr-1"></i>
+                          @endif
+                        @endif
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                        @if ($auction->bid === null)
+                            <form id="{{ $data->id }}" action="#" method="POST">
+                                @csrf
+                                @method('delete')
+                              </form>
+                              <a href="#" onclick="deleteConfirm('{{ $data->name }}', '{{ $data->id }}')"><i class="fas fa-crown text-green-400 mr-1"></i></a>
+                            @else
+                              @if ($auction->bid->people->name === $data->people->name)
+                              Dipilih
+                              @else
+                              Tidak dipilih
+                              @endif
+                        @endif
+                        </td>
+                    </tr>
+                    @endforeach            
+                    <!-- More items... -->
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            </div>
+        </div>
+        {{-- {{ $datas->links() }} --}}
+          </div>
+      </div>
   </div>
 </div>
   
